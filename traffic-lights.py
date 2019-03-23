d649ccf8f964aa7912dc648023a9eb9da1251da5
Dima007-k-stem-dev <48621074+Dima007-k-stem-dev@users.noreply.github.com>
2019-03-23 13:42:22 +0200

    Create traffic-lights.py

---
+++ traffic-lights.py
@@ -0,0 +1,12 @@
+from ev3dev2.led import Leds
+Dmitry=Leds()
+while True:
+  lights.set_color("LEFT", "RED")
+  lights.set_color("RIGHT", "RED")
+  time.sleep(1)
+  lights.set_color("LEFT", "AMBER")
+  lights.set_color("RIGHT", "AMBER")
+  time.sleep(1)
+  lights.set_color("LEFT", "GREEN")
+  lights.set_color("RIGHT", "GREEN")
+  time.sleep(1)
